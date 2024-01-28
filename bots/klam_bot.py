1536e66fa6495ddccc4c09234f33a0ba4296d8bf
StarFlower00000 <seal654321@gmail.com>
2024-01-27 23:07:53 -0500

    klam bomber mostly

--- bots/klam_bot.py
+++ bots/klam_bot.py
@@ -9,17 +9,25 @@ import math
 class BotPlayer(Player):
     def __init__(self, map: Map):
         self.map = map
-        self.first = self.mostpath(60, True, 1)[0]
-        
+        self.sniperpositions = self.mostpath(60, True, self.map.width*self.map.height)
+        self.bomberpositions = self.mostpath(10, True, self.map.width*self.map.height)
+        self.sunpositions = self.mostpath(60, False, self.map.width*self.map.height)
 
     def play_turn(self, rc: RobotController):
         self.build_towers(rc)
-        #self.towers_attack(rc)
+        self.towers_attack(rc)
 
     def build_towers(self, rc: RobotController):
-        location = self.first
-        if (rc.can_build_tower(TowerType.GUNSHIP, self.indextorow(location), self.indextocol(self.indextocol(location)))):
-            rc.build_tower(TowerType.GUNSHIP, self.indextorow(location), self.indextocol(self.indextocol(location)))
+        locations = self.sniperpositions.index(max(self.sniperpositions))
+        locationb = self.bomberpositions.index(max(self.bomberpositions))
+        if(max(self.bomberpositions)*3>max(self.sniperpositions)):
+            if (rc.can_build_tower(TowerType.BOMBER, self.indextorow(locationb), self.indextocol(locationb))):
+                rc.build_tower(TowerType.BOMBER, self.indextorow(locationb), self.indextocol(locationb))
+                self.bomberpositions[locationb] = -1
+        else:
+            if (rc.can_build_tower(TowerType.GUNSHIP, self.indextorow(locations), self.indextocol(locations))):
+                rc.build_tower(TowerType.GUNSHIP, self.indextorow(locations), self.indextocol(locations))
+                self.sniperpositions[locations] = -1
                 
     def indextorow(self, index:int):
         return index//self.map.width
@@ -29,22 +37,33 @@ class BotPlayer(Player):
     def mostpath(self, radius: int, is_max: bool, numreturn: int):
         path = []
         for location in range(self.map.width *self.map.height):
-            numberpath = 0
             row = self.indextorow(location)
             col = self.indextocol(location)
-            for a in range(2*radius):
-                for b in range(2*radius):
-                    if math.dist([a-8,b-8], [0, 0])< 8:
-                        if self.map.is_path(row+a-8,col+b-8):
-                            numberpath = numberpath + 1
-            path.append(numberpath)
-        returningstuff = []
+            if(self.map.is_space(row, col)):
+                numberpath = 0
+                for a in range(2*radius):
+                    for b in range(2*radius):
+                        if math.dist([a-8,b-8], [0, 0])< 8:
+                            if self.map.is_path(row+a-8,col+b-8):
+                                numberpath = numberpath + 1
+                path.append(numberpath)
+            else:
+                if is_max:
+                    path.append(-1)
+                else:
+                    path.append(48*48+1)
+        """returningstuff = []
         for i in range(numreturn):
             if is_max:
-                returningstuff.append(path.pop(path.index(max(path))))
+                returningstuff.append(path.index(max(path)))
+                path.pop(path.index(max(path)))
             else:
-                returningstuff.append(path.pop(path.index(min(path))))
-        return returningstuff
+                returningstuff.append(path.index(min(path)))
+                path.pop(path.index(min(path)))
+        print(returningstuff)
+        """
+        print(path)
+        return path
             
 
     
